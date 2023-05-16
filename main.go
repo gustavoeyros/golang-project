a9d919e576e6aa1ed9513e6fd269fb5104df58d7
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-16 11:49:51 -0300

    feat: createUser structure without db

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/gin-gonic/gin"
+	"github.com/gustavoeyros/golang-project/src/controllers/routes"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +14,11 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	fmt.Println(os.Getenv("env"))
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -1,5 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/gustavoeyros/golang-project/src/configurations/rest_err"
+	"github.com/gustavoeyros/golang-project/src/models/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var UserRequest request.UserRequest
+	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Invalid JSON body, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(UserRequest)
+}
