af0298d240767c4c7d7c969088f2510e188d9421
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-18 12:51:02 -0300

    feat: custom error validation

---
+++ src/configurations/validation/validate_user.go
@@ -0,0 +1,49 @@
+package validation
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
+	"github.com/gustavoeyros/golang-project/src/configurations/rest_err"
+)
+
+var (
+	Validate = validator.New()
+	transl   ut.Translator
+)
+
+func init() {
+	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		en := en.New()
+		unt := ut.New(en, en)
+		transl, _ = unt.GetTranslator("en")
+		en_translations.RegisterDefaultTranslations(val, transl)
+	}
+}
+
+func ValidateError(validation_err error) *rest_err.RestErr {
+	var jsonErr *json.UnmarshalTypeError
+	var jsonValidationError validator.ValidationErrors
+
+	if errors.As(validation_err, &jsonErr) {
+		return rest_err.NewBadRequestError("Invalid field type")
+	} else if errors.As(validation_err, &jsonValidationError) {
+		errorsCauses := []rest_err.Causes{}
+
+		for _, e := range validation_err.(validator.ValidationErrors) {
+			cause := rest_err.Causes{
+				Message: e.Translate(transl),
+				Field:   e.Field(),
+			}
+			errorsCauses = append(errorsCauses, cause)
+		}
+		return rest_err.NewBadRequestValidationError("Some fields are invalid", errorsCauses)
+	} else {
+		return rest_err.NewBadRequestError("Error trying to convert fields")
+	}
+}
--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -2,17 +2,19 @@ package controllers
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gustavoeyros/golang-project/src/configurations/rest_err"
+	"github.com/gustavoeyros/golang-project/src/configurations/validation"
 	"github.com/gustavoeyros/golang-project/src/models/request"
 )
 
 func CreateUser(c *gin.Context) {
 	var UserRequest request.UserRequest
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Invalid JSON body, error=%s\n", err.Error()))
-		c.JSON(restErr.Code, restErr)
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := validation.ValidateError(err)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 	fmt.Println(UserRequest)
